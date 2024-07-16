695797eb0be3083a50abfc0982e74c79628022ae
CyberRoshan <roshanchaurasia990@gmail.com>
2024-07-16 16:12:22 +0530

    Add GSAP Animations to CodeBase

--- script.js
+++ script.js
@@ -21,3 +21,46 @@ document.addEventListener("scroll",()=>{
     }
 })
 // ! Position Arrow End
+
+//  ! GSAP Features Start
+gsap.from("header",{
+    y:-300,
+    duration:1.5,
+    delay:0.2,
+    ease: "bounce"
+})
+
+gsap.from(".mid-aboutus > h2",{
+    y:100,
+    duration:1,
+    ease: "bounce",
+    scrollTrigger: "#About-me",
+    trigger:"top top"
+})
+gsap.from(".mid-hero--section h1",{
+    x:-100,
+    duration:1,
+})
+gsap.from(".mid-hero--section > p",{
+    x: 100,
+    duration:1
+})
+gsap.from(".btn-box",{
+    y:100,
+    duration:1
+})
+gsap.from(".about-left img",{
+    x: -800,
+    duration:1,
+    scrollTrigger:"#About-me",
+    trigger:"top top"
+})
+
+gsap.from(".about-right",{
+    x: 1200,
+    duration: 2,
+    delay:0.5,
+    scrollTrigger:"#About-me",
+    trigger: "top top"
+})
+//  ! GSAP Features End
